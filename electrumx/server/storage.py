7ee48d4b203a99720f68666615b4281f86a7276a
itserg <itserg2008@gmail.com>
2019-04-29 22:38:06 +0300

    Communicatinon with remote rocksdb

--- electrumx/server/storage.py
+++ electrumx/server/storage.py
@@ -8,10 +8,11 @@
 '''Backend database abstraction.'''
 
 import os
+import socket
 from functools import partial
 
 import electrumx.lib.util as util
-
+import electrumx.server.messages_pb2 as messages_pb2
 
 def db_class(name):
     '''Returns a DB engine class.'''
@@ -26,13 +27,16 @@ class Storage(object):
     '''Abstract base class of the DB backend abstraction.'''
 
     def __init__(self, name, for_sync):
-        self.is_new = not os.path.exists(name)
+        self.is_new = self.check_is_new()
+        self.open(name, True)        
         self.for_sync = for_sync or self.is_new
-        self.open(name, create=self.is_new)
 
     @classmethod
     def import_module(cls):
         '''Import the DB engine module.'''
+        raise NotImplementedError
+
+    def check_is_new(self):
         raise NotImplementedError
 
     def open(self, name, create):
@@ -68,99 +72,187 @@ class Storage(object):
         raise NotImplementedError
 
 
-class LevelDB(Storage):
-    '''LevelDB database engine.'''
-
-    @classmethod
-    def import_module(cls):
-        import plyvel
-        cls.module = plyvel
-
-    def open(self, name, create):
-        mof = 512 if self.for_sync else 128
-        # Use snappy compression (the default)
-        self.db = self.module.DB(name, create_if_missing=create,
-                                 max_open_files=mof)
-        self.close = self.db.close
-        self.get = self.db.get
-        self.put = self.db.put
-        self.iterator = self.db.iterator
-        self.write_batch = partial(self.db.write_batch, transaction=True,
-                                   sync=True)
-
-
 class RocksDB(Storage):
-    '''RocksDB database engine.'''
+
+    def __init__(self, name, for_sync):
+        self.storename = name        
+        super().__init__(name, for_sync)                        
 
     @classmethod
     def import_module(cls):
-        import rocksdb
-        cls.module = rocksdb
+        pass
 
-    def open(self, name, create):
-        mof = 512 if self.for_sync else 128
-        # Use snappy compression (the default)
-        options = self.module.Options(create_if_missing=create,
-                                      use_fsync=True,
-                                      target_file_size_base=33554432,
-                                      max_open_files=mof)
-        self.db = self.module.DB(name, options)
-        self.get = self.db.get
-        self.put = self.db.put
+    def check_is_new(self):
+        return not os.path.exists("/home/udev/tcprocks/rocksdata/" + self.storename)
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(("localhost", 3333))
+
+    def open(self, name, create):        
+        self.connect()
+
+        requestMessage = messages_pb2.OpenStore()
+        requestMessage.name = self.storename
+        msg = b'O' + requestMessage.SerializeToString()
+
+        self.socket.send(len(msg).to_bytes(4, byteorder='big'))
+        self.socket.send(msg)
+
+        dataSizeBuf = self.socket.recv(4)        
+        dataSize = int.from_bytes(dataSizeBuf, byteorder='big')
+        self.socket.recv(dataSize)
+
 
     def close(self):
-        # PyRocksDB doesn't provide a close method; hopefully this is enough
-        self.db = self.get = self.put = None
-        import gc
-        gc.collect()
+        self.socket.close()
+
+    def get(self, key):
+        requestMessage = messages_pb2.ByteArrayMessage()
+        requestMessage.storename= self.storename
+        requestMessage.data = key
+        msg = b'G' + requestMessage.SerializeToString()
+
+
+        self.socket.send(len(msg).to_bytes(4, byteorder='big'))
+        self.socket.send(msg)
+
+        dataSizeBuf = self.socket.recv(4)
+        dataSize = int.from_bytes(dataSizeBuf, byteorder='big')
+        data = self.socket.recv(dataSize)
+
+
+        resultMessage = messages_pb2.GetResultMessage()
+        resultMessage.ParseFromString(data)
+
+        if resultMessage.found == 1:
+            return resultMessage.data
+        else:
+            return None
+
+    def put(self, key, value):
+        requestMessage = messages_pb2.KeyValuePair()
+        requestMessage.storename= self.storename
+        requestMessage.key = key
+        requestMessage.val = value
+        msg = b'S' + requestMessage.SerializeToString()
+
+        self.socket.send(len(msg).to_bytes(4, byteorder='big'))
+        self.socket.send(msg)
+
+        dataSizeBuf = self.socket.recv(4)
+        dataSize = int.from_bytes(dataSizeBuf, byteorder='big')
+        self.socket.recv(dataSize)
+
 
     def write_batch(self):
-        return RocksDBWriteBatch(self.db)
+        return RocksDBWriteBatch(self)
 
     def iterator(self, prefix=b'', reverse=False):
-        return RocksDBIterator(self.db, prefix, reverse)
+        iter = RocksDBIterator(self, prefix, reverse)
+        return iter
+
+
+class RocksDBIterator(object):
+    '''An iterator for MongoDB.'''
+
+    def __init__(self, db, prefix, reverse):
+        self.db = db
+        self.prefix = prefix
+        self.reverse = reverse
+        self.loadData()
+
+    def __iter__(self):
+        return self
+
+    def loadData(self):        
+        requestMessage = messages_pb2.ByteArrayMessage()
+        requestMessage.storename = self.db.storename
+        requestMessage.data = self.prefix
+        msg = b'P' + requestMessage.SerializeToString()
+
+        self.db.socket.send(len(msg).to_bytes(4, byteorder='big'))
+        self.db.socket.send(msg)
+
+        dataSizeBuf = self.db.socket.recv(4)
+        dataSize = int.from_bytes(dataSizeBuf, byteorder='big')
+        data = self.db.socket.recv(dataSize)
+
+        resultMessage = messages_pb2.SearchByPrefixResponse()
+        resultMessage.ParseFromString(data)
+        self.result = resultMessage.vals
+
+
+
+        if self.reverse:
+            self.index = len(self.result) - 1
+        else:
+            self.index = 0
+        pass
+
+    def __next__(self):
+
+        if self.reverse:
+            if self.index <= 0:
+                raise StopIteration
+            else:
+                self.index -= 1
+                l = self.result[self.index + 1]
+                return (l.key, l.val)
+        else:
+            if (self.index) >= len(self.result) or len(self.result) == 0:
+                raise StopIteration
+            else:
+                self.index += 1
+                l = self.result[self.index - 1]
+                return (l.key, l.val)
+
+
+class RocksDBWriteBatchWriter(object):
+
+    def __init__(self, db):
+        self.db = db
+        self.result = []
+
+    def put(self, key, value):
+        pair = messages_pb2.BatchElement()
+        pair.key = key
+        pair.val = value
+        pair.operation = messages_pb2.BatchElement.UPDATE
+        self.result.append(pair)
+
+    def delete(self, key):
+        pair = messages_pb2.BatchElement()
+        pair.key = key
+        pair.operation = messages_pb2.BatchElement.DELETE
+        self.result.append(pair)
 
 
 class RocksDBWriteBatch(object):
     '''A write batch for RocksDB.'''
 
     def __init__(self, db):
-        self.batch = RocksDB.module.WriteBatch()
-        self.db = db
+        self.batch = RocksDBWriteBatchWriter(db)
+        self.db = db        
 
     def __enter__(self):
         return self.batch
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if not exc_val:
-            self.db.write(self.batch)
+        # pass
+        if not exc_val:            
+            # here should be async write
+            requestMessage = messages_pb2.BatchCommand()
+            requestMessage.storename = self.db.storename
+            requestMessage.vals.extend(self.batch.result)
+            msg = b'B' + requestMessage.SerializeToString() 
+            print('Sending batch len=' + str(len(msg)))
+            self.db.socket.send(len(msg).to_bytes(4, byteorder='big'))
+            self.db.socket.send(msg)
 
+            dataSizeBuf = self.db.socket.recv(4)
+            dataSize = int.from_bytes(dataSizeBuf, byteorder='big')
+            self.db.socket.recv(dataSize)
+            print('Batch is processed len=' + str(dataSize))
 
-class RocksDBIterator(object):
-    '''An iterator for RocksDB.'''
-
-    def __init__(self, db, prefix, reverse):
-        self.prefix = prefix
-        if reverse:
-            self.iterator = reversed(db.iteritems())
-            nxt_prefix = util.increment_byte_string(prefix)
-            if nxt_prefix:
-                self.iterator.seek(nxt_prefix)
-                try:
-                    next(self.iterator)
-                except StopIteration:
-                    self.iterator.seek(nxt_prefix)
-            else:
-                self.iterator.seek_to_last()
-        else:
-            self.iterator = db.iteritems()
-            self.iterator.seek(prefix)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        k, v = next(self.iterator)
-        if not k.startswith(self.prefix):
-            raise StopIteration
-        return k, v
+            self.batch.result.clear()
